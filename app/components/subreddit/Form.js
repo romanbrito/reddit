3b949aceefb9a441c71344d9dd32e2fa773c7c8f
Roman Brito <romanbrito1@gmail.com>
2017-06-16 23:25:37 -0500

    material ui form

--- app/components/subreddit/Form.js
+++ app/components/subreddit/Form.js
@@ -2,47 +2,73 @@ import React, {Component} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 injectTapEventPlugin();
 
 class Form extends Component {
 
-  handleChange = () => {
-    return (event) => {
-      let state = {};
-      const name = event.target.name;
-      const value = event.target.value;
-      this.setState({
-        [name]: value
-      });
-    }
+  state = {
+    title: '',
+    content: ''
+  };
+
+  // When a user submits...
+  handleSubmit = (event) => {
+    // prevent the HTML from trying to submit a form if the user hits "Enter" instead of
+    // clicking the button
+    event.preventDefault();
+    console.log('title ' + this.state.title);
+    console.log('content ' + this.state.content);
+
+  };
+
+  handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
   };
 
   render() {
-    return(
-      <form>
-        <label>
-          Is going:
-          <input
-            name="isGoing"
-            type="string"
-            //checked={this.state.isGoing}
-            onChange={this.handleChange}/>
-        </label>
+    return (
+      <form onSubmit={this.handleSubmit}>
+
+        <MuiThemeProvider>
+          <TextField
+            hintText="Title"
+            floatingLabelText="Title"
+            name="title"
+            type="text"
+            value={this.state.title}
+            onChange={this.handleChange}
+          />
+        </MuiThemeProvider>
         <br />
-        <label>
-          Number of guests:
-          <input
-            name="numberOfGuests"
-            type="number"
-            //value={this.state.numberOfGuests}
-            onChange={this.handleChange}/>
-        </label>
+
+        <MuiThemeProvider>
+          <TextField
+            hintText="Content"
+            floatingLabelText="Content"
+            multiLine={true}
+            rows={2}
+            name="content"
+            type="text"
+            value={this.state.content}
+            onChange={this.handleChange}
+          />
+        </MuiThemeProvider>
+        <br />
+
         <MuiThemeProvider>
           <RaisedButton
-            label={'Test Button'}
+            label={'submit'}
             primary={true}
-            onTouchTap={()=>{console.log('hello, I work')}}
+            onTouchTap={() => {
+              console.log('it works')
+            }}
+            type="submit"
           />
         </MuiThemeProvider>
       </form>
