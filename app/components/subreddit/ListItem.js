51dc4ddc8ea4db376233816f62691fe505d3861b
Roman Brito <romanbrito1@gmail.com>
2017-06-17 17:40:40 -0500

    link to post ready, working on routes

--- app/components/subreddit/ListItem.js
+++ app/components/subreddit/ListItem.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router';
 
 export default class ListItem extends Component {
 	render() {
 		return (
 			<li>
+
+				<Link to={"/" + this.props.post.subredditId + this.props.post._id}>
+
 				<h2>{this.props.post.title}</h2>
 				<p>{this.props.post.content}</p>
+
+        </Link>
 			</li>
 		);
 	}
